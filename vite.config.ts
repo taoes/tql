dc42eee4d05be53e719898d65ac4539da70c7020
Tao <zhoutao3210@gmail.com>
2026-05-31 18:19:25 +0800

    init project

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react";
+
+
+const host = process.env.TAURI_DEV_HOST;
+
+
+export default defineConfig(async () => ({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+        protocol: "ws",
+        host,
+        port: 1421,
+      }
+      : undefined,
+    watch: {
+      // 3. tell Vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}));
